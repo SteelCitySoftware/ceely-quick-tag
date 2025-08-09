8816f1cd7e8ea902a13ef725cf1659b5ada39450
Chris <chrisadmin@MacBook-Pro.fios-router.home>
2025-08-09 14:54:20 -0400

    Update graphql query to pull by fulfillment

--- app/routes/order-export.query.ts
+++ app/routes/order-export.query.ts
@@ -1,41 +1,54 @@
 // GraphQL query for fetching a Shopify order by query string (order id or name)
 export const getOrderByQuery = `#graphql
   query getOrderByQuery($query: String!) {
-    orders(first: 1, query: $query) {
-      edges {
-        node {
-          id
-          name
-          customer {
-            displayName
-            quickbooksName: metafield(namespace: "custom", key: "quickbooks_name") {
-              value
-            }
+ query getOrderByQuery($query: String!) {
+  orders(first: 1, query: $query) {
+    edges {
+      node {
+        id
+        name
+        customer {
+          displayName
+          quickbooksName: metafield(namespace: "custom", key: "quickbooks_name") {
+            value
           }
-          createdAt
-          lineItems(first: 100) {
+        }
+        fulfillments {
+          id
+          status
+          name
+          trackingInfo {
+            company
+            number
+            url
+          }
+          fulfillmentOrders(first: 10) {
             edges {
               node {
-                title
-                quantity
-                currentQuantity
-                originalUnitPriceSet {
-                  shopMoney { amount }
-                }
-                variant {
-                  sku
-                  product {
-                    productType
+                lineItems(first: 250) {
+                  edges {
+                    node {
+                      id
+                      productTitle
+                      variantTitle
+                      totalQuantity
+                      variant {
+                        compareAtPrice
+                        price
+                        sku
+                        product {
+                          productType
+                        }
+                      }
+                    }
                   }
                 }
               }
             }
           }
-          customerPONumber: metafield(namespace: "custom", key: "customer_po_number") {
-            value
-          }
         }
       }
     }
   }
+}
 `;
