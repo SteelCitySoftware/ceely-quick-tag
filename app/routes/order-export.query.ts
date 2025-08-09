9c965a969aa60c9b458bc5f176b4910f9c1aada1
Chris <chrisadmin@MacBook-Pro.fios-router.home>
2025-08-09 14:58:12 -0400

    fix brackets

--- app/routes/order-export.query.ts
+++ app/routes/order-export.query.ts
@@ -1,43 +1,45 @@
 // GraphQL query for fetching a Shopify order by query string (order id or name)
 export const getOrderByQuery = `#graphql
   query getOrderByQuery($query: String!) {
- query getOrderByQuery($query: String!) {
-  orders(first: 1, query: $query) {
-    edges {
-      node {
-        id
-        name
-        customer {
-          displayName
-          quickbooksName: metafield(namespace: "custom", key: "quickbooks_name") {
-            value
-          }
-        }
-        fulfillments {
-          id
-          status
-          name
-          trackingInfo {
-            company
-            number
-            url
-          }
-          fulfillmentOrders(first: 10) {
-            edges {
-              node {
-                lineItems(first: 250) {
-                  edges {
-                    node {
-                      id
-                      productTitle
-                      variantTitle
-                      totalQuantity
-                      variant {
-                        compareAtPrice
-                        price
-                        sku
-                        product {
-                          productType
+    query getOrderByQuery($query: String!) {
+      orders(first: 1, query: $query) {
+        edges {
+          node {
+            id
+            name
+            customer {
+              displayName
+              quickbooksName: metafield(namespace: "custom", key: "quickbooks_name") {
+                value
+              }
+            }
+            fulfillments {
+              id
+              status
+              name
+              trackingInfo {
+                company
+                number
+                url
+              }
+              fulfillmentOrders(first: 10) {
+                edges {
+                  node {
+                    lineItems(first: 250) {
+                      edges {
+                        node {
+                          id
+                          productTitle
+                          variantTitle
+                          totalQuantity
+                          variant {
+                            compareAtPrice
+                            price
+                            sku
+                            product {
+                              productType
+                            }
+                          }
                         }
                       }
                     }
@@ -49,6 +51,5 @@ export const getOrderByQuery = `#graphql
         }
       }
     }
-  }
 }
 `;
