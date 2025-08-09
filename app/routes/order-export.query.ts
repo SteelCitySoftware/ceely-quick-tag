25c3fc1ba6d4779b57c3487d523398bb3e2a2dc6
Chris <chrisadmin@MacBook-Pro.fios-router.home>
2025-08-09 15:06:52 -0400

    Update order-export.query.ts

--- app/routes/order-export.query.ts
+++ app/routes/order-export.query.ts
@@ -1,43 +1,61 @@
 // GraphQL query for fetching a Shopify order by query string (order id or name)
 export const getOrderByQuery = `#graphql
   query getOrderByQuery($query: String!) {
-      orders(first: 1, query: $query) {
-        edges {
-          node {
-            id
-            name
-            customer {
-              displayName
-              quickbooksName: metafield(namespace: "custom", key: "quickbooks_name") {
-                value
+    orders(first: 1, query: $query) {
+      edges {
+        node {
+          id
+          name
+          customer {
+            displayName
+            quickbooksName: metafield(namespace: "custom", key: "quickbooks_name") {
+              value
+            }
+          }
+          createdAt
+          lineItems(first: 100) {
+            edges {
+              node {
+                title
+                quantity
+                currentQuantity
+                originalUnitPriceSet {
+                  shopMoney { amount }
+                }
+                variant {
+                  sku
+                  product {
+                    productType
+                  }
+                }
               }
             }
-            fulfillments {
-              id
-              status
-              name
-              trackingInfo {
-                company
-                number
-                url
-              }
-              fulfillmentOrders(first: 10) {
-                edges {
-                  node {
-                    lineItems(first: 250) {
-                      edges {
-                        node {
-                          id
-                          productTitle
-                          variantTitle
-                          totalQuantity
-                          variant {
-                            compareAtPrice
-                            price
-                            sku
-                            product {
-                              productType
-                            }
+          }
+          fulfillments {
+            id
+            status
+            name
+            trackingInfo {
+              company
+              number
+              url
+            }
+            fulfillmentOrders(first: 10) {
+              edges {
+                node {
+                  lineItems(first: 250) {
+                    edges {
+                      node {
+                        id
+                        productTitle
+                        variantTitle
+                        totalQuantity
+                        variant {
+                          compareAtPrice
+                          price
+                          sku
+                          product {
+                            productType
                           }
                         }
                       }
@@ -47,7 +65,11 @@ export const getOrderByQuery = `#graphql
               }
             }
           }
+          customerPONumber: metafield(namespace: "custom", key: "customer_po_number") {
+            value
+          }
         }
       }
     }
+  }
 `;
